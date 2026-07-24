49f5894c45b55dc92469f15c6df114d8a4c18a75
aaneleh <helenakurzgon@gmail.com>
2026-07-23 22:11:24 -0300

    feat: IA Chat

--- src/pages/Chat/index.tsx
+++ src/pages/Chat/index.tsx
@@ -1,23 +1,94 @@
+import { useEffect, useRef, useState, type SetStateAction } from 'react';
 import './styles.css'
 import { useTranslation } from "react-i18next";
+import { useTasks } from '../../contexts/tasksContext';
+import type { Task } from '../../@types/task';
+import OpenAI from 'openai';
+import { FiSend } from "react-icons/fi";
+import i18next from 'i18next';
 
 function Chat() {
 
+  const openAI_API = import.meta.env.VITE_OPENAI_API_KEY;
+
+  const client = new OpenAI({
+    apiKey: openAI_API,
+    dangerouslyAllowBrowser: true
+  });
+
   const { t } = useTranslation();
+  const { tasks } = useTasks() as {tasks: Task[]};
+  const [messages, setMessages] = useState<Array<{ role: string; content: string }>>([]);
+  const [inputValue, setInputValue] = useState("");
+  const messagesEndRef = useRef<HTMLElement | null>(null);
+
+  const instructions = `
+  Usar linguagem informal e cotidiana. 
+  Não utilizar efeitos como negrito ou itálico. 
+  Utilize o idioma atual: ${i18next.resolvedLanguage}
+  `;
+
+  const scrollToBottom = () => {
+    messagesEndRef.current?.scrollIntoView({
+      behavior: "smooth"
+    })
+  }
+
+  useEffect(scrollToBottom, [messages]);
+
+  const handleChange = (e: { currentTarget: { value: SetStateAction<string>; }; }) => {
+    setInputValue(e.currentTarget.value)
+  }
+  const handleEnter = (e: { key: string; preventDefault: () => void; }) => {
+    if(e.key === "Enter"){
+      e.preventDefault();
+      sendMessage()
+    }
+  }
+
+  const sendMessage = () => {
+    messages.push({
+        role: "user",
+        content: inputValue,
+    })
+    setInputValue('');
+    getResponse();
+  }
+
+  const getResponse = async () => {
+    const response = await client.responses.create({
+      model: 'gpt-5.4-mini',
+      instructions: instructions,
+      input: messages
+    });
+    
+    setMessages((prev) => [
+      ...prev,
+      {
+        role: "assistant",
+        content: response.output_text,
+      }
+    ])
+  }
 
   return (
     <section id="chat">
         <h2>Chat</h2>
         <div className="chat-content">
-          <p className="chat ai-chat">
-            Hello World
-          </p>
-                  <p className="chat ai-chat">
-            Eaerr
-          </p>
+          {
+            messages.map((el, key) => (
+              <div key={key} className={`${el.role} message-wrapper`}>
+                <p className='message'>{el.content}</p>
+              </div>
+            ))
+          }
         </div>
         <div className="chat-input">
-          NOVA MENSAGEM
+          <input type="text" value={inputValue} placeholder='Envie sua mensagem' onChange={handleChange}  onKeyDown={handleEnter}/>
+
+          <button className='button' onClick={sendMessage} disabled={inputValue == ''}>
+            <FiSend />
+          </button>
         </div>
     </section>
   )
